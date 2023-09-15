5c37171cf33709f43df764dfd887efdc9f74fb3b
haeun82 <cjwd829@gmail.com>
2023-09-15 16:52:35 +0900

    스타벅스 페이지_추가_23.09.15

--- js/main.js
+++ js/main.js
@@ -127,5 +127,30 @@ promotionToggleBtn.addEventListener('click', function () {
   }
 });
 
+// 유튜브 섹션 위에 부유 요소 애니메이션 처리
+// gsap.to(요소, 지속시간, 옵션: {}) 메소드: css속성을 통해 애니메이션 처리
+// 옵션 참고: https://greensock.com/docs/v3/GSAP/gsap.to()
+gsap.to('.floating1', 1.5, {
+  dalay: 1, // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+  y: 15, // 수직으로 얼마나 움직일지 설정 ( translateY(수치)와 같음 )
+  repeat: -1, // 몇 번 반복하는지를 설정, -1은 무한 반복임
+  yoyo: true, // 한 번 재생된 애니메이션을 다시 뒤로 재생하는 것
+  ease: Power1.easeInOut // 타이밍 함수 적용, 느리게-빠르게-느리게
+});
 
+gsap.to('.floating2', 2, {
+  dalay: 0.5, 
+  y: 15, 
+  repeat: -1, 
+  yoyo: true, 
+  ease: Power1.easeInOut 
+});
+
+gsap.to('.floating3', 2.5, {
+  dalay: 2, 
+  y: 20, 
+  repeat: -1, 
+  yoyo: true, 
+  ease: Power1.easeInOut 
+});
 
